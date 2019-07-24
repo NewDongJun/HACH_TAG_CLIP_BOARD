65b643c3abb5c0b811d771ee94346ed6227ace0b
cmne3602 <53000757+cmne3602@users.noreply.github.com>
2019-07-24 20:02:13 +0900

    Add files via upload
    list로

---
+++ 안녕.py
@@ -0,0 +1,24 @@
+A = []
+
+def check(a, b) :
+    for i in range(0, len(a)-1 ) :
+        if a[i][0] == b :
+            return i
+    return -1
+    
+def new() :
+
+    tag = input("hashtag :")
+    
+    x = check(A, tag)
+    
+    if x == -1 :
+        A.append([tag])
+        
+    text = input("text :")
+    A[x].append(text)
+    
+while 1 :
+
+    new()
+    print (A)
